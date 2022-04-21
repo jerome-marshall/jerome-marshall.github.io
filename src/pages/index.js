810c820de948eaa5bf387c65b0a5e1481ae36de9
Jerome Marsall <jeromemarshall0@gmail.com>
2022-04-21 23:41:16 +0530

    ✨ NEW: basic theme change setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from "react"
+import { useTheme } from "next-themes"
 
 const Home = () => {
+  const { theme, setTheme } = useTheme()
+
   return (
-    <div className='flex'>Home</div>
+    <div className="flex bg-slate-300 dark:bg-slate-900 w-full h-screen transition-all delay-500">
+      Home
+      <button
+        aria-label="Toggle Dark Mode"
+        type="button"
+        className="p-3 h-12 w-12 order-2 md:order-3"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >Toggle</button>
+    </div>
   )
 }
 
