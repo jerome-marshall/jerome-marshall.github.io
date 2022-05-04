8ced373b668c540ca7ebd4601b48ab6c2705fecf
Jerome Marsall <jeromemarshall0@gmail.com>
2022-05-04 18:31:20 +0530

    ⚡ IMPROVE: prettier

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({children}) => {
-  return (
-    <button className="btn">{children}</button>
-  )
-}
+const Button = ({ children }) => {
+  return <button className="btn">{children}</button>;
+};
 
-export default Button
+export default Button;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,12 +13,22 @@ const Hero = () => {
     <div className="-mt-20 h-screen pt-20">
       <div className="flex h-full w-full flex-col items-center justify-center ">
         <div className="flex flex-col">
-          <p className="text-secondary dark:text-dark-secondary text-base">{heroData.pageTitle}</p>
-          <h1 className="text-text_900 dark:text-dark-text_900 text-[38px] font-bold">{data.name}.</h1>
-          <h3 className="text-text_700 dark:text-dark-text_700 text-[28px] font-bold">{heroData.shortDescription}</h3>
-          <p className="text-text_500 dark:text-dark-text_500 text-base mt-5">{heroData.description}</p>
+          <p className="text-base text-secondary dark:text-dark-secondary">
+            {heroData.pageTitle}
+          </p>
+          <h1 className="text-[38px] font-bold text-text_900 dark:text-dark-text_900">
+            {data.name}.
+          </h1>
+          <h3 className="text-[28px] font-bold text-text_700 dark:text-dark-text_700">
+            {heroData.shortDescription}
+          </h3>
+          <p className="mt-5 text-base text-text_500 dark:text-dark-text_500">
+            {heroData.description}
+          </p>
         </div>
-        <div className="mt-12"><Button>Get in Touch</Button></div>
+        <div className="mt-12">
+          <Button>Get in Touch</Button>
+        </div>
       </div>
     </div>
   );
