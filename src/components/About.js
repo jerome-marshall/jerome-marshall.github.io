4458f40b4d29ce25dbdb37ec48bc99fe5e22008e
Jerome Marsall <jeromemarshall0@gmail.com>
2022-05-05 10:56:08 +0530

    ⚡ IMPROVE: prettier refactor

--- src/components/About.js
+++ src/components/About.js
@@ -2,15 +2,13 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../data/GlobalContext";
 
 const About = () => {
-    const { data } = useContext(GlobalContext);
-    const aboutData = data.pageContent.find(
-        (content) => content.__component === "page-content.about-page-content"
-    );
-    console.log("🚀 ~ file: About.js ~ line 9 ~ About ~ aboutData", aboutData)
+  const { data } = useContext(GlobalContext);
+  const aboutData = data.pageContent.find(
+    (content) => content.__component === "page-content.about-page-content"
+  );
+  console.log("🚀 ~ file: About.js ~ line 9 ~ About ~ aboutData", aboutData);
 
-  return (
-    <div className="h-screen">About</div>
-  )
-}
+  return <div className="h-screen">About</div>;
+};
 
-export default About
+export default About;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,35 @@
 const customColors = {
   accent_border: "#efd199",
-      secondary: "#815400",
-      primary: "#d78c00",
-      accent_hover: "#ad7000",
-      background_1: "#fbf4e6",
-      background_2: "#f9edd6",
-      background_3: "#f1e2af",
-      background_hover: "#111111",
-      text_900: "#100e00",
-      text_700: "#65522e",
-      text_500: "#9f875c",
-      text_hover: "#eeeeee",
-      text_accent: "#d78c00",
-      shadow: "#111111",
-      dark: {
-        accent_border: "#102e66",
-        secondary: "#7eabff",
-        primary: "#2873ff",
-        accent_hover: "#528fff",
-        background_1: "#040b19",
-        background_2: "#061229",
-        background_3: "#0e1d50",
-        background_hover: "#eeeeee",
-        text_900: "#eff1ff",
-        text_700: "#9aadd1",
-        text_500: "#6078a3",
-        text_hover: "#111111",
-        text_accent: "#2873ff",
-        shadow: "#eeeeee",
-      },
-}
+  secondary: "#815400",
+  primary: "#d78c00",
+  accent_hover: "#ad7000",
+  background_1: "#fbf4e6",
+  background_2: "#f9edd6",
+  background_3: "#f1e2af",
+  background_hover: "#111111",
+  text_900: "#100e00",
+  text_700: "#65522e",
+  text_500: "#9f875c",
+  text_hover: "#eeeeee",
+  text_accent: "#d78c00",
+  shadow: "#111111",
+  dark: {
+    accent_border: "#102e66",
+    secondary: "#7eabff",
+    primary: "#2873ff",
+    accent_hover: "#528fff",
+    background_1: "#040b19",
+    background_2: "#061229",
+    background_3: "#0e1d50",
+    background_hover: "#eeeeee",
+    text_900: "#eff1ff",
+    text_700: "#9aadd1",
+    text_500: "#6078a3",
+    text_hover: "#111111",
+    text_accent: "#2873ff",
+    shadow: "#eeeeee",
+  },
+};
 
 module.exports = {
   darkMode: "class",
@@ -37,25 +37,25 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'toggle-shadow': `0 0 35px 1px ${customColors.primary}`,
-        'dark-toggle-shadow': `0 0 35px 1px ${customColors.dark.primary}`,
-      }
+        "toggle-shadow": `0 0 35px 1px ${customColors.primary}`,
+        "dark-toggle-shadow": `0 0 35px 1px ${customColors.dark.primary}`,
+      },
     },
     fontFamily: {
       "noto-sans": ["Noto Sans JP", "sans-serif"],
     },
     colors: {
-      ...customColors
+      ...customColors,
     },
   },
   plugins: [
-    function({ addBase, theme }) {
-      function extractColorVars(colorObj, colorGroup = '') {
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = "") {
         return Object.keys(colorObj).reduce((vars, colorKey) => {
           const value = colorObj[colorKey];
 
           const newVars =
-            typeof value === 'string'
+            typeof value === "string"
               ? { [`--color${colorGroup}-${colorKey}`]: value }
               : extractColorVars(value, `-${colorKey}`);
 
@@ -64,7 +64,7 @@ module.exports = {
       }
 
       addBase({
-        ':root': extractColorVars(theme('colors')),
+        ":root": extractColorVars(theme("colors")),
       });
     },
   ],
