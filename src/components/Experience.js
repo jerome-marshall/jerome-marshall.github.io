ac73bbc00ec6f42862c4ef963d5c906b1c02d7a7
Jerome Marshall <jeromemarshall0@gmail.com>
2022-05-28 19:18:11 +0530

    ✨ NEW: finished lg:experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,6 +3,7 @@ import { GlobalContext } from "../data/GlobalContext";
 import { Tab, Disclosure, Transition } from "@headlessui/react";
 import ReactMarkdown from "react-markdown";
 import ExpAccordian from "./ExpAccordian";
+import { classNames } from "../utils/utils";
 
 const Experience = () => {
   const { data } = useContext(GlobalContext);
@@ -23,46 +24,74 @@ const Experience = () => {
   };
 
   return (
-    <div className="container" id="experience">
-      <div className="flex h-full flex-col items-center py-20">
-        <h3 className="">{expData.pageTitle}</h3>
+    <div className="container max-w-[800px]" id="experience">
+      <div className="flex h-full w-full flex-col items-center py-20">
+        <h3 className="self-start">{expData.pageTitle}</h3>
         <div className="mt-12">
-          <Tab.Group vertical as="div" className="flex">
+          <Tab.Group vertical as="div" className="hidden  md:flex">
             <Tab.List className="flex flex-col">
               {expData.jobs.map((job, i) => (
-                <Tab key={job.companyName + "Tab"}>{job.companyName}</Tab>
+                <Tab
+                  key={job.companyName + "Tab"}
+                  className={({ selected }) =>
+                    classNames(
+                      "mr-8 whitespace-nowrap border-l-[3px] p-3 text-left text-sm hover:bg-background_3 hover:text-accent_hover dark:hover:bg-dark-background_3 dark:hover:text-dark-accent_hover",
+                      selected
+                        ? "border-secondary text-secondary dark:border-dark-secondary  dark:text-dark-secondary"
+                        : "border-accent_border text-text_500 dark:border-dark-accent_border dark:text-dark-text_500"
+                    )
+                  }
+                >
+                  {job.companyName}
+                </Tab>
               ))}
             </Tab.List>
             <Tab.Panels>
               {expData.jobs.map((job, i) => (
                 <Tab.Panel key={job.companyName + "Panel"}>
-                  <h4 className="">
-                    {job.designation} <span className="">@</span>{" "}
-                    <span className="">{job.companyName}</span>
+                  <h4 className="text-xl">
+                    <span className="font-bold text-text_900 dark:text-dark-text_900">
+                      {job.designation}
+                    </span>{" "}
+                    <span className="text-secondary dark:text-dark-secondary">
+                      @
+                    </span>{" "}
+                    <a
+                      className="font-bold text-primary dark:text-dark-primary"
+                      href={job.url}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {job.companyName}
+                    </a>
                   </h4>
-                  <p className="">{job.range}</p>
-                  <ReactMarkdown>{job.workDescription}</ReactMarkdown>
+                  <p className="mt-1 text-sm text-text_500 dark:text-dark-text_500">
+                    {job.range}
+                  </p>
+                  <ReactMarkdown className="mt-6 text-text_700 descendant-li:mb-2 descendant-li:leading-loose dark:text-dark-text_700">
+                    {job.workDescription}
+                  </ReactMarkdown>
                 </Tab.Panel>
               ))}
             </Tab.Panels>
           </Tab.Group>
+          <div className="flex w-full flex-col overflow-hidden rounded-xl border-2 border-background_3 dark:border-dark-background_3 md:hidden">
+            {expData.jobs.map((job, i) => {
+              return (
+                <ExpAccordian
+                  key={job.companyName}
+                  length={expData.jobs.length}
+                  pannelRef={pannelRef}
+                  openedDisclosure={openedDisclosure}
+                  toggleDisclosure={toggleDisclosure}
+                  job={job}
+                  i={i}
+                ></ExpAccordian>
+              );
+            })}
+          </div>
         </div>
         {/* accordian */}
-        <div className="mt-12 flex w-full flex-col overflow-hidden rounded-xl border-2 border-background_3 dark:border-dark-background_3 md:hidden">
-          {expData.jobs.map((job, i) => {
-            return (
-              <ExpAccordian
-                key={job.companyName}
-                length={expData.jobs.length}
-                pannelRef={pannelRef}
-                openedDisclosure={openedDisclosure}
-                toggleDisclosure={toggleDisclosure}
-                job={job}
-                i={i}
-              ></ExpAccordian>
-            );
-          })}
-        </div>
       </div>
     </div>
   );
