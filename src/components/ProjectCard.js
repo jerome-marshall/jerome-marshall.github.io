db7d2e6b5969c46b7e2f12f1a4227ce186443af9
Jerome Marshall <jeromemarshall0@gmail.com>
2022-06-07 22:15:37 +0530

    ✨ NEW: projects section animation

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,51 +3,73 @@ import { FiGithub as IconGitHub } from "react-icons/fi";
 import { FiExternalLink as IconExternal } from "react-icons/fi";
 import { FiFolder as IconFolder } from "react-icons/fi";
 import SkillLink from "./SkillLink";
+import { motion } from "framer-motion";
 
-const ProjectCard = ({ project }) => {
-  console.log(
-    "🚀 ~ file: ProjectCard.js ~ line 8 ~ ProjectCard ~ project",
-    project
-  );
+const ProjectCard = ({ project, index }) => {
+  const containerVariant = {
+    hidden: {
+      opacity: 0,
+      y: 40,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+        ease: "easeInOut",
+        delay: 0.2 + index * 0.2,
+        when: "beforeChildren",
+      },
+    },
+  };
+
   return (
-    <div className="group z-10 flex w-full flex-col bg-background_2 px-5 py-7 transition-all duration-300 hover:-translate-y-2 dark:bg-dark-background_2">
-      <div className="flex items-center justify-between text-text_700 child-svg:h-9 child-svg:w-9 dark:text-dark-text_700">
-        <IconFolder />
-        <div className=" z-10 flex gap-4 descendant-svg:h-6 descendant-svg:w-6 descendant-svg:cursor-pointer descendant-svg:transition-all descendant-svg:duration-300">
-          {project.githubURL && (
-            <a href={project.githubURL} target="_blank" rel="noreferrer">
-              <IconGitHub className="hover:text-text_900 dark:hover:text-dark-text_900" />
-            </a>
-          )}
-          {project.externalURL && (
-            <a href={project.externalURL} target="_blank" rel="noreferrer">
-              <IconExternal className="hover:text-text_900 dark:hover:text-dark-text_900" />
-            </a>
-          )}
+    <motion.div
+      className="z-10"
+      variants={containerVariant}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+    >
+      <div className="group z-10 flex h-full flex-col bg-background_2 px-5 py-7 transition-all duration-300 hover:-translate-y-2 dark:bg-dark-background_2">
+        <div className="flex items-center justify-between text-text_700 child-svg:h-9 child-svg:w-9 dark:text-dark-text_700">
+          <IconFolder />
+          <div className=" z-10 flex gap-4 descendant-svg:h-6 descendant-svg:w-6 descendant-svg:cursor-pointer descendant-svg:transition-all descendant-svg:duration-300">
+            {project.githubURL && (
+              <a href={project.githubURL} target="_blank" rel="noreferrer">
+                <IconGitHub className="hover:text-text_900 dark:hover:text-dark-text_900" />
+              </a>
+            )}
+            {project.externalURL && (
+              <a href={project.externalURL} target="_blank" rel="noreferrer">
+                <IconExternal className="hover:text-text_900 dark:hover:text-dark-text_900" />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="my-7 flex flex-col">
+          <h5 className="text-xl font-bold text-text_900 transition-all duration-300 group-hover:text-primary  dark:text-dark-text_900 dark:group-hover:text-dark-primary">
+            {project.name}
+          </h5>
+          <p className="mt-3 text-text_500 dark:text-dark-text_500 lg:text-sm">
+            {project.description}
+          </p>
+        </div>
+        <div className="-mb-3 mt-auto flex w-full gap-4 overflow-auto pb-3">
+          {project.techStack?.map((skill) => (
+            <div
+              className="inline before:content-[''] first:ml-auto"
+              key={skill.name}
+            >
+              <SkillLink
+                skill={skill}
+                className=" text-xs text-text_700 transition-all duration-300 group-hover:text-primary  dark:text-dark-text_700 dark:group-hover:text-dark-primary"
+              />
+            </div>
+          ))}
         </div>
       </div>
-      <div className="my-7 flex flex-col">
-        <h5 className="text-xl font-bold text-text_900 transition-all duration-300 group-hover:text-primary  dark:text-dark-text_900 dark:group-hover:text-dark-primary">
-          {project.name}
-        </h5>
-        <p className="mt-3 text-text_500 dark:text-dark-text_500 lg:text-sm">
-          {project.description}
-        </p>
-      </div>
-      <div className="-mb-3 mt-auto flex w-full gap-4 overflow-auto pb-3">
-        {project.techStack?.map((skill) => (
-          <div
-            className="inline before:content-[''] first:ml-auto"
-            key={skill.name}
-          >
-            <SkillLink
-              skill={skill}
-              className=" text-xs text-text_700 transition-all duration-300 group-hover:text-primary  dark:text-dark-text_700 dark:group-hover:text-dark-primary"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,7 @@ import { FiExternalLink as IconExternal } from "react-icons/fi";
 import { FiFolder as IconFolder } from "react-icons/fi";
 import SkillLink from "./SkillLink";
 import ProjectCard from "./ProjectCard";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   const { data } = useContext(GlobalContext);
@@ -25,16 +26,43 @@ const Projects = () => {
     speed: 500,
   };
 
+  const containerVariant = {
+    hidden: {
+      opacity: 0,
+      y: 100,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+        ease: "easeInOut",
+        // delay: 1,
+        when: "beforeChildren",
+      },
+    },
+  };
+
   return (
     <div className="container pt-20" id="projects">
-      <div className="flex h-full w-full flex-col items-center justify-center">
+      <motion.div
+        className="flex h-full w-full flex-col items-center justify-center"
+        variants={containerVariant}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+      >
         <h3 className="text-4xl">{projectsData.pageTitle}</h3>
         <div className="mt-14 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-5">
           {projects.map((project, index) => (
-            <ProjectCard project={project} key={project.name + index} />
+            <ProjectCard
+              project={project}
+              key={project.name + index}
+              index={index}
+            />
           ))}
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
