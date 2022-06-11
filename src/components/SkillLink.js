b075f79f29b12cb19facfce14218a729f098085e
Jerome Marshall <jeromemarshall0@gmail.com>
2022-06-11 22:08:23 +0530

    🌈 STYLE: added skillLink hover

--- src/components/SkillLink.js
+++ src/components/SkillLink.js
@@ -3,7 +3,7 @@ import React from "react";
 const SkillLink = ({ skill, className }) => {
   return (
     <a
-      className={`whitespace-nowrap text-sm ${className}`}
+      className={`whitespace-nowrap text-sm transition-all duration-300 hover:text-primary dark:hover:text-dark-secondary ${className}`}
       href={skill.url}
       target="_blank"
       rel="noopener noreferrer"
