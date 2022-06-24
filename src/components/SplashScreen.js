ab78fd456bd9751c8ba43abb7b4f4c1f06f691b5
Jerome Marshall <jeromemarshall0@gmail.com>
2022-06-24 17:43:11 +0530

    ⚡ IMPROVE: splash icon animation

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useAnimation } from "framer-motion";
 import { FaQuoteLeft as IconQuote } from "react-icons/fa";
 import { BsArrowRightCircle as IconGo } from "react-icons/bs";
 
 const SplashScreen = ({ setIsLoading }) => {
   const [showContine, setShowContinue] = useState(false);
+  const [startIconAniamation, setStartIconAniamation] = useState(false);
+
+  const iconControls = useAnimation();
 
   setTimeout(() => {
     setShowContinue(true);
-  }, 4000);
+  }, 5000);
 
   const wrapperVariant = {
     hidden: {
@@ -59,32 +62,6 @@ const SplashScreen = ({ setIsLoading }) => {
       },
     },
   };
-  const iconDivVarient = {
-    hidden: {
-      opacity: 0,
-      height: "0px",
-    },
-    visible: {
-      opacity: 1,
-      height: "40px",
-
-      transition: {
-        duration: 1,
-        ease: "easeInOut",
-        // staggerChildren: 0.1,
-        // delayChildren: 0.2,
-      },
-    },
-    exit: {
-      opacity: 0,
-      transition: {
-        duration: 0.5,
-        ease: "easeInOut",
-        // staggerChildren: 0.1,
-        // delayChildren: 0.2,
-      },
-    },
-  };
   const iconGoVarient = {
     hidden: {
       opacity: 0,
@@ -98,6 +75,15 @@ const SplashScreen = ({ setIsLoading }) => {
       transition: {
         duration: 1,
         ease: "easeInOut",
+      },
+    },
+    animation: {
+      opacity: 1,
+      x: 0,
+      scale: 1.05,
+      transition: {
+        duration: 1,
+        yoyo: Infinity,
       },
     },
     hover: {
@@ -139,19 +125,26 @@ const SplashScreen = ({ setIsLoading }) => {
               quas odit est libero ipsam alias eius tempora, impedit aliquid ex
             </p>
             <motion.div
-              className={`mt-14 mb-8 flex w-40 items-center justify-center ${
-                showContine ? "h-24" : "h-2"
+              className={` flex w-40 items-center justify-center ${
+                showContine ? "mt-10 mb-10 h-24" : "mt-14 mb-8 h-2"
               } transition-all duration-1000`}
             >
-              <AnimatePresence exitBeforeEnter>
+              <AnimatePresence
+                exitBeforeEnter
+                onExitComplete={() => {
+                  setTimeout(() => {
+                    setStartIconAniamation(true);
+                  }, 1000);
+                }}
+              >
                 {showContine ? (
                   <motion.div
-                    className="h-full w-full cursor-pointer text-dark-secondary child-svg:h-full child-svg:w-full"
+                    className="h-full w-full cursor-pointer text-dark-secondary hover:text-dark-primary child-svg:h-full child-svg:w-full child-svg:transition-all child-svg:duration-500 "
                     variants={iconGoVarient}
                     initial="hidden"
-                    animate="visible"
+                    animate={!startIconAniamation ? "visible" : "animation"}
                     key="icon-go"
-                    whileHover="hover"
+                    // whileHover="hover"
                     layout
                     onClick={() => setIsLoading(false)}
                   >
