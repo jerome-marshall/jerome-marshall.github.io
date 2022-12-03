5dadec3df512b6b6b5fb9387e40b25db0195fa51
Jerome Marshall <jeromemarshall0@gmail.com>
2022-12-04 00:30:36 +0530

    🚧 WIP: change data source to new strapi 4

--- src/data/graphql-client.js
+++ src/data/graphql-client.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://portfolio-strapi3-api.herokuapp.com/graphql",
+  uri: process.env.STRAPI_GRAPHQL,
   cache: new InMemoryCache(),
 });
 
@@ -11,99 +11,86 @@ export const getGlobalData = async () => {
     query: gql`
       query getData {
         globalDatum {
-          id
-          name
-          socials {
-            id
-            name
-            displayName
-            url
-          }
-          pageContent {
-            ... on ComponentPageContentHeroPageContent {
-              __typename
-              id
-              pageTitle
-              shortDescription
-              description
-            }
-            ... on ComponentPageContentAboutPageContent {
-              __typename
-              id
-              pageTitle
-              description
-              skillset {
+          data {
+            attributes {
+              name
+              shortIntroduction
+              introduction
+              about
+              skillsHighlight {
+                data {
+                  attributes {
+                    name
+                    url
+                  }
+                }
+              }
+              contactText
+              projectsHighlight {
+                data {
+                  attributes {
+                    name
+                    date
+                    githubURL
+                    externalURL
+                    projectFor
+                    personal
+                    description
+                    images {
+                      data {
+                        attributes {
+                          url
+                        }
+                      }
+                    }
+                    shortDescription
+                    techStack {
+                      data {
+                        attributes {
+                          name
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+              image {
+                data {
+                  attributes {
+                    url
+                  }
+                }
+              }
+              socials {
                 id
                 name
                 url
               }
             }
-            ... on ComponentPageContentExperiencePageContent {
-              __typename
-              id
-              pageTitle
-              jobs {
-                id
-                companyName
-                designation
-                doj
-                range
-                url
-                workDescription
-              }
-            }
-            ... on ComponentPageContentProjectsPageContent {
-              __typename
-              id
-              pageTitle
-              projects {
-                name
-                date
-                githubURL
-                externalURL
-                projectFor
-                personal
-                description
-                projectImg {
-                  url
-                }
-                techStack {
-                  name
-                  name
-                  url
-                }
-              }
-            }
-            ... on ComponentPageContentContactPageCotent {
-              __typename
-              id
-              pageTitleIntro
-              pageTitle
-              description
-            }
           }
-        }
-        quotes {
-          author
-          quote
         }
       }
     `,
   });
 
-  return response.data.globalDatum;
+  return response.data.globalDatum.data.attributes;
 };
+
 export const getQuotes = async () => {
   const response = await client.query({
     query: gql`
       query Quotes {
         quotes {
-          author
-          quote
+          data {
+            attributes {
+              quote
+              author
+            }
+          }
         }
       }
     `,
   });
 
-  return response.data.quotes;
+  return response.data.quotes?.data.map((quote) => quote.attributes);
 };
