ee4b5a4d69737ca82d089beb5701dd89464f6615
Jerome Marshall <jeromemarshall0@gmail.com>
2022-12-05 00:10:27 +0530

    🐞 FIX: Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
+import GoogleAnalytics from "../components/GoogleAnalytics";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
+      <GoogleAnalytics mID={"G-K0X6YXQ2PG"} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
