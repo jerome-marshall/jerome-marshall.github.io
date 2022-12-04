1032e90ea2b7b4729066ba8329c82a66d987a213
Jerome Marshall <jeromemarshall0@gmail.com>
2022-12-04 22:08:03 +0530

    🔨 DEPRECATE: remove dynamic imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { AnimatePresence } from "framer-motion";
-import { Suspense, useState } from "react";
+import { useState } from "react";
 import About from "../components/About";
 import Experience from "../components/Experience";
 import Hero from "../components/Hero";
@@ -12,7 +12,6 @@ import SideBar from "../components/SideBar";
 import SplashScreen from "../components/SplashScreen";
 import { getGlobalData, getJobs, getQuotes } from "../data/graphql-client";
 import { GlobalDatum, Job, Quote } from "../types/types";
-import dynamic from "next/dynamic";
 
 const Home = ({
   data,
@@ -23,25 +22,9 @@ const Home = ({
   quotes: Quote[];
   jobs: Job[];
 }) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-
-  const DynamicHero = dynamic(() => import("../components/Hero"), {
-    suspense: true,
-  });
-  const DynamicAbout = dynamic(() => import("../components/About"), {
-    suspense: true,
-  });
-  const DynamicExperience = dynamic(() => import("../components/Experience"), {
-    suspense: true,
-  });
-  const DynamicProjects = dynamic(() => import("../components/Projects"), {
-    suspense: true,
-  });
-  const DynamicContact = dynamic(() => import("../components/Contact"), {
-    suspense: true,
-  });
 
   return (
     <div className="bg-background_1 dark:bg-dark-background_1">
@@ -60,23 +43,18 @@ const Home = ({
             randomQuote={randomQuote}
           />
         ) : (
-          <Suspense>
-            <Layout data={data}>
-              <DynamicHero
-                name={data.name}
-                shortIntro={data.shortIntroduction}
-                introduction={data.introduction}
-              />
-              <DynamicAbout
-                aboutMe={data.about}
-                skillsData={data.skillsHighlight}
-              />
-              <DynamicExperience jobs={jobs} />
-              <DynamicProjects projectsData={data.projectsHighlight} />
-              <DynamicContact content={data.contactText} />
-              <SideBar socials={data.socials} />
-            </Layout>
-          </Suspense>
+          <Layout data={data}>
+            <Hero
+              name={data.name}
+              shortIntro={data.shortIntroduction}
+              introduction={data.introduction}
+            />
+            <About aboutMe={data.about} skillsData={data.skillsHighlight} />
+            <Experience jobs={jobs} />
+            <Projects projectsData={data.projectsHighlight} />
+            <Contact content={data.contactText} />
+            <SideBar socials={data.socials} />
+          </Layout>
         )}
       </AnimatePresence>
     </div>
