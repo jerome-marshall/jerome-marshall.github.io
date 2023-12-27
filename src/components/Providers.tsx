f59bfe1eff55b607cb4ea1aa16a3f2d484edb5e5
Jerome Marshall <jeromemarshall0@gmail.com>
2023-12-27 12:38:42 +0530

    ✨ NEW: posthog analytics

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,13 +3,18 @@
 import React from "react";
 import type { FC } from "react";
 import { ThemeProvider } from "next-themes";
+import PHProvider from "./PHProvider";
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 const Providers: FC<ProvidersProps> = ({ children }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class">
+      <PHProvider>{children}</PHProvider>
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
